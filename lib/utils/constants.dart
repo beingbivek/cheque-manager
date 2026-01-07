3000c882c418e19622aa0127620dad1ba634d848
Bivek Thapa <beingbivek@gmail.com>
2026-01-07 19:36:41 +0545

    changing public key

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,4 @@
 class AppConstants {
   // Khalti PUBLIC key (safe to expose)
-  static const String khaltiPublicKey = "5cb32431f50a4ec890360954db917aaf";
+  static const String khaltiPublicKey = "live_public_key_5cb32431f50a4ec890360954db917aaf";
 }
